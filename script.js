1d19056fef911a45fffdf87c4ecb8452d7d50c3e
Khushi Srivastava <srivastavakhushi615@gmail.com>
2026-02-24 09:30:53 +0530

    Primitive datatypes

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-console.log("Hello World");
-document.write("Hello,World");
+var num=10;
+console.log(num);
+console.log(typeof(num));
+
+num=false;
+console.log(num);
+console.log(typeof(num));
+
+num="Khushi Srivastava";
+console.log(num);
+console.log(typeof(num));
+
+num=null;
+console.log(num);
+console.log(typeof(num));
+
+num=undefined;
+console.log(num);
+console.log(typeof(num));
