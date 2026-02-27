e23abce1f14e261c5d789fe3693858fb784694ca
Khushi Srivastava <srivastavakhushi615@gmail.com>
2026-02-27 12:09:27 +0530

    switch case and if else

--- script5.js
+++ script5.js
@@ -5,3 +5,20 @@ if(age>=18) {
 else {
     console.log("You are a Minor");
 }
+
+var grade='A';
+document.write("Entering switch block<br/>");
+switch(grade){
+    case 'A': document.write("Good job <br/>");
+    break;
+    case 'B': document.write("Pretty good <br/>");
+    break;
+    case 'C': document.write("Passed <br/>");
+    break;
+    case 'D': document.write("Not so good <br/>");
+    break;
+    case 'E': document.write("Failed <br/>");
+    break;
+    default:document.write("Unknown grade <br/>")
+}
+document.write("Existing switch block");
