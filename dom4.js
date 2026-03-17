bf7a686ce22b492466ef61fdcee5497e6ea5f35e
Khushi Srivastava <srivastavakhushi615@gmail.com>
2026-03-17 09:47:06 +0530

    events

---
+++ dom4.js
@@ -0,0 +1,18 @@
+function onClick(){
+    console.log("clicked");
+}
+function onMouseOver(){
+    console.log("on mouse over");
+}
+function onMouseOut(){
+    console.log("on mouse out");
+}
+function onMouseUp(){
+    console.log("on mouse up");
+}
+function onMouseDown(){
+    console.log("on mouse down");
+}
+function onMouseMove(){
+    console.log("on mouse move");
+}
